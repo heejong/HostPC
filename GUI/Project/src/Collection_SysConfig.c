c0644b8da8e50e94b115c53238eb3964ef1fc2f0
gnetscher <gnetscher@gmail.com>
2013-07-15 12:02:21 -0700

    initialized sys config tree with checkboxes

--- GUI/Project/src/Collection_SysConfig.c
+++ GUI/Project/src/Collection_SysConfig.c
@@ -6,6 +6,7 @@
 extern int panelHandle_calibration;
 extern Stack panel_stack;   // defined in UI_Common  
 extern OpenPETTree current_location;
+extern OpenPETTree sys_config;  
 
 int CVICALLBACK SaveSetup (int panel, int control, int event,
 		void *callbackData, int eventData1, int eventData2)
@@ -31,12 +32,12 @@ int CVICALLBACK SingleEventChange (int panel, int control, int event,
 			GetCtrlIndex (panel, control, &ring_index);
 			if(ring_index == 0) {
 				// 32-bit single event word
-				SetCtrlAttribute (panel, TABPANEL_COINCIDENCE_32_RING, ATTR_VISIBLE, 1);
-				SetCtrlAttribute (panel, TABPANEL_COINCIDENCE_64_RING, ATTR_VISIBLE, 0); 
+				SetCtrlAttribute (panel, SYSCONFIG_COINCIDENCE_32_RING, ATTR_VISIBLE, 1);
+				SetCtrlAttribute (panel, SYSCONFIG_COINCIDENCE_64_RING, ATTR_VISIBLE, 0); 
 			} else {
 				// 64-bit single event word
-				SetCtrlAttribute (panel, TABPANEL_COINCIDENCE_64_RING, ATTR_VISIBLE, 1);
-				SetCtrlAttribute (panel, TABPANEL_COINCIDENCE_32_RING, ATTR_VISIBLE, 0);
+				SetCtrlAttribute (panel, SYSCONFIG_COINCIDENCE_64_RING, ATTR_VISIBLE, 1);
+				SetCtrlAttribute (panel, SYSCONFIG_COINCIDENCE_32_RING, ATTR_VISIBLE, 0);
 			}
 			break;
 	}
@@ -57,3 +58,97 @@ int CVICALLBACK SysConfigTree (int panel, int control, int event,
 }
 
 // initialize tree control
+int CVICALLBACK InitializeSysConfig (int panel, int event, void *callbackData,
+		int eventData1, int eventData2)
+{
+	char title_string[50];
+	char item_label_root[15], item_label[15],item_label_MB[5], item_label_DUC[5], item_label_DB[5];
+	int idx_MB, idx_DUC, idx_DB, i, j, k;
+	int num_items=1;
+	int current_boards[3], idx=0;
+	
+	switch (event)
+	{
+		case EVENT_GOT_FOCUS:
+			// populate instrument tree
+			GetNumListItems (panel, SYSCONFIG_TREE, &num_items);
+			if(num_items==1) 
+			{
+				CheckListItem (panel, SYSCONFIG_TREE, 0, 1);
+				// need to create table
+				for(i=0; i<=sys_config.MB; i++)
+				{
+					//add MB as child
+					sprintf(item_label_MB, "MB%d", i);
+					strcpy(item_label, item_label_MB);	  // tag = MB0
+					idx_MB = InsertTreeItem (panel, SYSCONFIG_TREE, VAL_CHILD, 0, VAL_LAST, item_label_MB, item_label, 0, num_items++);
+					CheckListItem (panel, SYSCONFIG_TREE, idx_MB, 1);
+		
+					for(j=0; j<=sys_config.DUC; j++)	  // assumes same number of DUC into each MB
+					{
+						//add DUC as child
+						sprintf(item_label_DUC, "DUC%d", j);
+						strcat(item_label, item_label_DUC);   // tag = MB0DUC0
+						strcpy(item_label_root, item_label);
+						idx_DUC = InsertTreeItem (panel, SYSCONFIG_TREE, VAL_CHILD, idx_MB, VAL_LAST, item_label_DUC, item_label, 0, num_items++);
+						CheckListItem (panel, SYSCONFIG_TREE, idx_DUC, 1);
+						
+						for(k=0; k<=sys_config.DB; k++)		 // assumes same number of DB into each DUC
+						{
+							//add DB as child
+							sprintf(item_label_DB, "DB%d", k); 
+							strcat(item_label, item_label_DB);   // tag = MB0DUC0DB0
+							idx_DB = InsertTreeItem (panel, SYSCONFIG_TREE, VAL_CHILD, idx_DUC, VAL_LAST, item_label_DB, item_label, 0, num_items++);
+							strcpy(item_label,item_label_root);   // tag = MB0DUC0
+							if(k == current_location.DB)
+								SetTreeItemAttribute (panel, SYSCONFIG_TREE, idx_DB, ATTR_SELECTED, 1);
+								CheckListItem (panel, SYSCONFIG_TREE, idx_DB, 1);
+						}
+						strcpy(item_label,item_label_MB);   // tag = MB0
+						if(j != current_location.DUC)
+							SetTreeItemAttribute (panel, SYSCONFIG_TREE, idx_DUC, ATTR_COLLAPSED, 1);
+			
+					}
+					strcpy(item_label,"");   // tag = ""
+					if(i != current_location.MB)
+						SetTreeItemAttribute (panel, SYSCONFIG_TREE, idx_MB, ATTR_COLLAPSED, 1);
+				}
+			} 
+			else 
+			{
+				// table already created, just need to show correct location
+				for(i=1; i<num_items; i++)
+				{
+					// collapse all
+					SetTreeItemAttribute (panel, SYSCONFIG_TREE, i, ATTR_COLLAPSED, 1);	
+				}
+
+				if(current_location.MB != -1)
+				{
+					sprintf(item_label, "MB%d", current_location.MB);
+					GetTreeItemFromTag (panel, SYSCONFIG_TREE, item_label, &idx_MB);
+					SetTreeItemAttribute (panel, SYSCONFIG_TREE, idx_MB, ATTR_COLLAPSED, 0);	
+				} 
+				if(current_location.DUC != -1)
+				{
+					sprintf(item_label, "MB%dDUC%d", current_location.MB, current_location.DUC);
+					GetTreeItemFromTag (panel, SYSCONFIG_TREE, item_label, &idx_DUC);
+					SetTreeItemAttribute (panel, SYSCONFIG_TREE, idx_DUC, ATTR_COLLAPSED, 0);	
+				} 
+				if(current_location.DB != -1)
+				{
+					sprintf(item_label, "MB%dDUC%dDB%d", current_location.MB, current_location.DUC, current_location.DB);
+					GetTreeItemFromTag (panel, SYSCONFIG_TREE, item_label, &idx_DB);
+					SetTreeItemAttribute (panel, SYSCONFIG_TREE, idx_DB, ATTR_SELECTED, 1);
+				} 
+			}
+			break;
+		case EVENT_LOST_FOCUS:
+
+			break;
+		case EVENT_CLOSE:
+
+			break;
+	}
+	return 0;
+}
--- GUI/Project/ui/Collection_SysConfig.h
+++ GUI/Project/ui/Collection_SysConfig.h
@@ -14,28 +14,23 @@
 
      /* Panels and Controls: */
 
-#define  SYSCONFIG                        1
-#define  SYSCONFIG_COMMANDBUTTON_12       2       /* control type: command, callback function: SaveSetup */
-#define  SYSCONFIG_COMMANDBUTTON_13       3       /* control type: command, callback function: Quit */
-#define  SYSCONFIG_COMMANDBUTTON_14       4       /* control type: command, callback function: Back */
-#define  SYSCONFIG_COMMANDBUTTON_15       5       /* control type: command, callback function: Main */
-#define  SYSCONFIG_TEXTMSG                6       /* control type: textMsg, callback function: (none) */
-#define  SYSCONFIG_DECORATION_9           7       /* control type: deco, callback function: (none) */
-#define  SYSCONFIG_TAB                    8       /* control type: tab, callback function: (none) */
-
-     /* tab page panel controls */
-#define  TABPANEL_SUBTITLE2               2       /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_COINCIDENCE_64_RING     3       /* control type: ring, callback function: (none) */
-#define  TABPANEL_SUBTITLE1               4       /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_COINCIDENCE_32_RING     5       /* control type: ring, callback function: (none) */
-#define  TABPANEL_FILETEXT                6       /* control type: string, callback function: (none) */
-#define  TABPANEL_DECORATION_17           7       /* control type: deco, callback function: (none) */
-#define  TABPANEL_LOAD                    8       /* control type: command, callback function: (none) */
-#define  TABPANEL_DECORATION_16           9       /* control type: deco, callback function: (none) */
-#define  TABPANEL_SINGLEEVENT_RING        10      /* control type: ring, callback function: SingleEventChange */
-
-     /* tab page panel controls */
-#define  TABPANEL_2_TREE                  2       /* control type: tree, callback function: SysConfigTree */
+#define  SYSCONFIG                        1       /* callback function: InitializeSysConfig */
+#define  SYSCONFIG_TREE                   2       /* control type: tree, callback function: SysConfigTree */
+#define  SYSCONFIG_SUBTITLE2              3       /* control type: textMsg, callback function: (none) */
+#define  SYSCONFIG_COINCIDENCE_64_RING    4       /* control type: ring, callback function: (none) */
+#define  SYSCONFIG_SUBTITLE1              5       /* control type: textMsg, callback function: (none) */
+#define  SYSCONFIG_COINCIDENCE_32_RING    6       /* control type: ring, callback function: (none) */
+#define  SYSCONFIG_FILETEXT               7       /* control type: string, callback function: (none) */
+#define  SYSCONFIG_DECORATION_17          8       /* control type: deco, callback function: (none) */
+#define  SYSCONFIG_LOAD                   9       /* control type: command, callback function: (none) */
+#define  SYSCONFIG_DECORATION_16          10      /* control type: deco, callback function: (none) */
+#define  SYSCONFIG_SINGLEEVENT_RING       11      /* control type: ring, callback function: SingleEventChange */
+#define  SYSCONFIG_COMMANDBUTTON_12       12      /* control type: command, callback function: SaveSetup */
+#define  SYSCONFIG_COMMANDBUTTON_13       13      /* control type: command, callback function: Quit */
+#define  SYSCONFIG_COMMANDBUTTON_14       14      /* control type: command, callback function: Back */
+#define  SYSCONFIG_COMMANDBUTTON_15       15      /* control type: command, callback function: Main */
+#define  SYSCONFIG_TEXTMSG                16      /* control type: textMsg, callback function: (none) */
+#define  SYSCONFIG_DECORATION_9           17      /* control type: deco, callback function: (none) */
 
 
      /* Menu Bars, Menus, and Menu Items: */
@@ -46,6 +41,7 @@
      /* Callback Prototypes: */
 
 int  CVICALLBACK Back(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK InitializeSysConfig(int panel, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Main(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Quit(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK SaveSetup(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
